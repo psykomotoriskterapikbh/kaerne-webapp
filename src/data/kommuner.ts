64c36af47bd13cc732ddad7378dde4378d09d723
psykomotoriskterapikbh <psykomotoriskterapikbh@gmail.com>
2026-06-10 14:40:28 +0200

    Create kommuner.ts

---
+++ src/data/kommuner.ts
@@ -0,0 +1,23 @@
+// Danmarks 98 kommuner. Kilde: Dataforsyningen (offentlig DAGI-data), juni 2026.
+// Bruges til kommune-vælgeren i brugerprofilen.
+
+export const KOMMUNER: string[] = [
+  "Aabenraa", "Aalborg", "Aarhus", "Albertslund", "Allerød", "Assens",
+  "Ballerup", "Billund", "Bornholm", "Brøndby", "Brønderslev",
+  "Dragør", "Egedal", "Esbjerg", "Fanø", "Favrskov", "Faxe",
+  "Faaborg-Midtfyn", "Fredensborg", "Fredericia", "Frederiksberg",
+  "Frederikshavn", "Frederikssund", "Furesø", "Gentofte", "Gladsaxe",
+  "Glostrup", "Greve", "Gribskov", "Guldborgsund", "Haderslev",
+  "Halsnæs", "Hedensted", "Helsingør", "Herlev", "Herning", "Hillerød",
+  "Hjørring", "Holbæk", "Holstebro", "Horsens", "Hvidovre",
+  "Høje-Taastrup", "Hørsholm", "Ikast-Brande", "Ishøj", "Jammerbugt",
+  "Kalundborg", "Kerteminde", "Kolding", "København", "Køge",
+  "Langeland", "Lejre", "Lemvig", "Lolland", "Lyngby-Taarbæk", "Læsø",
+  "Mariagerfjord", "Middelfart", "Morsø", "Norddjurs", "Nordfyns",
+  "Nyborg", "Næstved", "Odder", "Odense", "Odsherred", "Randers",
+  "Rebild", "Ringkøbing-Skjern", "Ringsted", "Roskilde", "Rudersdal",
+  "Rødovre", "Samsø", "Silkeborg", "Skanderborg", "Skive", "Slagelse",
+  "Solrød", "Sorø", "Stevns", "Struer", "Svendborg", "Syddjurs",
+  "Sønderborg", "Thisted", "Tønder", "Tårnby", "Vallensbæk", "Varde",
+  "Vejen", "Vejle", "Vesthimmerlands", "Viborg", "Vordingborg", "Ærø",
+];
